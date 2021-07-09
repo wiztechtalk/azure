7da030fadb749637a284134b5afdf492ce13cb84
wiztechtalk <samuelmitch@gmail.com>
2021-07-09 04:43:53 -0500

    Update AZ-104_ps.ps1
    remove the track changes and leave the update code

--- AZ-104_ps.ps1
+++ AZ-104_ps.ps1
@@ -1,20 +1,6 @@
 Connect-AzAccount -UseDeviceAuthentication
 
 #Default Resource parameters
-$myRGname = "mypsRG1"
-$myLocation = "EastUS"
-$myVNETname = "mypsVNET10-1"
-
-#Linux VM parameters
-$LinuxVMSize = "Standard_B1ls"
-$LinuxImage = "UbuntuLTS"
-
-#Windows VM parameters
-$WinVMSize = "Standard_B1s"
-$WinImage = "WindowsServer2019Datacenter"Connect-AzAccount -UseDeviceAuthentication
-
-#Default Resource parameters
-<<<<<<< HEAD
 $myRGname = Read-host -Prompt "Enter the Resource Group" # "mypsRG2"
 $myLocation = Read-host -Prompt "Enter the Location (e.g. eastus)" # "EastUS"
 $myVNETname = Read-host -Prompt "Enter the VNet Name" # "mypsVNET10-1"
@@ -31,31 +17,14 @@ else {
         $VMSize = "Standard_B1s"
         $Image = "WindowsServer2019Datacenter"
         }
-=======
-$myRGname = "mypsRG1"
-$myLocation = "EastUS"
-$myVNETname = "mypsVNET10-1"
-
-#Linux VM parameters
-$LinuxVMSize = "Standard_B1ls"
-$LinuxImage = "UbuntuLTS"
-
-#Windows VM parameters
-$WinVMSize = "Standard_B1s"
-$WinImage = "WindowsServer2019Datacenter"
->>>>>>> 6623cb1a9d14b9ee7e6cc6521d4c6acd2460d829
 
 #Required Tag parameters
 $myTag = New-AzTag -Name "Department" -Value "IT"
 
 #Create the Resource Group
 New-AzResourceGroup -Name $myRGname `
-<<<<<<< HEAD
                     -Location $myLocation `
                     -Force
-=======
-                    -Location $myLocation
->>>>>>> 6623cb1a9d14b9ee7e6cc6521d4c6acd2460d829
 
 #Create the Subnets
 $mySubnet1 = New-AzVirtualNetworkSubnetConfig -Name "mypsSubnet0" `
@@ -69,12 +38,8 @@ New-AzVirtualNetwork -Name $myVNETname `
                     -Location $myLocation `
                     -ResourceGroupName $myRGname `
                     -AddressPrefix "10.1.0.0/16" `
-<<<<<<< HEAD
                     -Subnet $mySubnet1,$mySubnet2 `
                     -Force
-=======
-                    -Subnet $mySubnet1,$mySubnet2
->>>>>>> 6623cb1a9d14b9ee7e6cc6521d4c6acd2460d829
 
 # Create the VM
 New-AzVM -Name "mypsVM1" `
@@ -82,7 +47,6 @@ New-AzVM -Name "mypsVM1" `
         -Location $myLocation `
         -VirtualNetworkName $myVNETname `
         -SubnetName "mypsSubnet1" `
-<<<<<<< HEAD
         -Image $Image `
         -Size $VMSize `
         -AsJob -Confirm
@@ -107,48 +71,3 @@ $storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroupName `
 
 # Retrieve the context.
 $ctx = $storageAccount.Context
-=======
-        -Image $WinImage `
-        -Size $WinVMSize
-
-#New-AzVM -Name "test" -Location $myLocation -ResourceGroupName $myRGname
-
-# Clean up the resources
-Remove-AzResourceGroup $myRGname -Force -Verbose
->>>>>>> 6623cb1a9d14b9ee7e6cc6521d4c6acd2460d829
-
-
-#Required Tag parameters
-$myTag = New-AzTag -Name "Department" -Value "IT"
-
-#Create the Resource Group
-New-AzResourceGroup -Name $myRGname `
-                    -Location $myLocation
-
-#Create the Subnets
-$mySubnet1 = New-AzVirtualNetworkSubnetConfig -Name "mypsSubnet0" `
-                                            -AddressPrefix "10.1.0.0/24"
-
-$mySubnet2 = New-AzVirtualNetworkSubnetConfig -Name "mypsSubnet1" `
-                                            -AddressPrefix "10.1.1.0/24"
-
-#Create the VNet
-New-AzVirtualNetwork -Name $myVNETname `
-                    -Location $myLocation `
-                    -ResourceGroupName $myRGname `
-                    -AddressPrefix "10.1.0.0/16" `
-                    -Subnet $mySubnet1,$mySubnet2
-
-# Create the VM
-New-AzVM -Name "mypsVM1" `
-        -ResourceGroupName $myRGname `
-        -Location $myLocation `
-        -VirtualNetworkName $myVNETname `
-        -SubnetName "mypsSubnet1" `
-        -Image $WinImage `
-        -Size $WinVMSize
-
-#New-AzVM -Name "test" -Location $myLocation -ResourceGroupName $myRGname
-
-# Clean up the resources
-Remove-AzResourceGroup $myRGname -Force -Verbose
